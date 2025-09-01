dadda37966a826426a714d39db6c4e5a58d71d26
pr-th-m <pratham.dv@codecraft.co.in>
2025-09-01 14:24:48 +0530

    feat: commit 1

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 console.log("Set a date, just do it and you will see the differnece");
 console.log("Set a date, just do it and you will see the differnece");
+console.log("Set a date, just do it and you will see the differnece");
